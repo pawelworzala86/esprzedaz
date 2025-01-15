edde9a28e16ab9e51b0c0763deb8bc698290ca4e
Pawel Worzala <pawelworzala86@gmail.com>
2025-01-15 19:12:17 +0100

    refaktoring

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\FormularzController;
 use App\Http\Controllers\LoginController;
-
 use App\Http\Controllers\PetsController;
 
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-//Route::get('/nowy-endpoint', [NowyEndpointController::class, 'index']);
-Route::get('/nowy-endpoint', function () {
-    return view('welcome');
-});
-
-Route::post('/formularz', [FormularzController::class, 'store']);
-Route::get('/formularz', function () { return view('formularz'); });
 
 Route::get('/logowanie', function () { return view('login'); });
 Route::post('/api/user/login', [LoginController::class, 'login']);
 
 
-
-
+Route::get('/', [PetsController::class, 'table']);
 Route::get('/sklep', [PetsController::class, 'table']);
-//Route::get('/sklep/dodaj', function () { return view('addPet'); });
 Route::get('/sklep/dodaj', [PetsController::class, 'editPet']);
 Route::get('/sklep/edycja/{id}', [PetsController::class, 'editPet']);
 Route::post('/api/pets', [PetsController::class, 'addPet']);
